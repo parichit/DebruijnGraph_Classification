7e36eb8a5de90bdcab339b6b0a99939b66fda331
Parichit <parichitsharma@outlook.com>
2023-05-05 01:12:49 -0400

    STABLE: added CRAN mirror

--- ClassificationPipeline/InstallMissingPackages.R
+++ ClassificationPipeline/InstallMissingPackages.R
@@ -4,80 +4,82 @@
 
 install_missing_packages <- function(modelFilePath){
   
-alreadyInstalled = rownames(installed.packages())
-
-base_libs = list()
-missing_libs = c()
-lib_model_list = list()
-regressionModels = c()
-
-successPackages = c()
-failedPackage = c()
-successModels = c()
-failedModels = c()
-
-supp_libs = c("doParallel", "tidyverse", "caretEnsemble", "ggplot2", "ggthemes", 
+  alreadyInstalled = rownames(installed.packages())
+  
+  base_libs = list()
+  missing_libs = c()
+  lib_model_list = list()
+  regressionModels = c()
+  
+  successPackages = c()
+  failedPackage = c()
+  successModels = c()
+  failedModels = c()
+  
+  options(repos = c(CRAN = "https://cloud.r-project.org/"))
+  
+  supp_libs = c("doParallel", "tidyverse", "caretEnsemble", "ggplot2", "ggthemes", 
                 "xtable", "tictoc", "foreach", "R.utils", 
                 "readxl", "stringr", "dplyr", "magrittr", "gridExtra", "yardstick", "ROSE")
-
-# Install additional supporting libs
-for (i in 1:length(supp_libs)){
-  if (!(supp_libs[i] %in% alreadyInstalled)){
-    install.packages(supp_libs[i], dependencies = c("Imports", "Depends", "Suggests"))
+  
+  # Install additional supporting libs
+  for (i in 1:length(supp_libs)){
+    if (!(supp_libs[i] %in% alreadyInstalled)){
+      install.packages(supp_libs[i], dependencies = c("Imports", "Depends", "Suggests"))
+    }
   }
-}
-
-
-modelFile <- file(modelFilePath, open="r")
-contents <- readLines(modelFile)
-
-
-for (line in contents){
-  cols = unlist(strsplit(line, ","))
+  
+  
+  modelFile <- file(modelFilePath, open="r")
+  contents <- readLines(modelFile)
+  
+  
+  for (line in contents){
+    cols = unlist(strsplit(line, ","))
     for (i in 2:length(cols)){
       base_libs[[cols[1]]] = append(base_libs[[cols[1]]], cols[i]) 
       regressionModels = c(regressionModels, trimws(noquote(cols[i])))
     }
-}
-
-
-namesBaseLibs <- unique(names(base_libs))
-regressionModels = unique(regressionModels)
-
-
-# Find libraries that are missing on this system
-for (i in namesBaseLibs){
-  # Record missing packages
-  if (!(i %in% alreadyInstalled)){
-    missing_libs = c(missing_libs, i)
   }
-  else{
-    successPackages = c(successPackages, i)
-    successModels = c(successModels, trimws(noquote(unlist(base_libs[[i]]))))
+  
+  
+  namesBaseLibs <- unique(names(base_libs))
+  regressionModels = unique(regressionModels)
+  
+  
+  # Find libraries that are missing on this system
+  for (i in namesBaseLibs){
+    # Record missing packages
+    if (!(i %in% alreadyInstalled)){
+      missing_libs = c(missing_libs, i)
+    }
+    else{
+      successPackages = c(successPackages, i)
+      successModels = c(successModels, trimws(noquote(unlist(base_libs[[i]]))))
+    }
   }
-}
-
-print(paste("Total classification models: ", length(regressionModels), "Number of required libraries:", length(namesBaseLibs), 
-            "Number of missing libraries: ", length(missing_libs)))
-
-
-# Install the missing libraries and
-# count the number of covered packages
-
-if (length(missing_libs) > 1){
+  
+  print(paste("Total classification models: ", length(regressionModels), "Number of required libraries:", length(namesBaseLibs), 
+              "Number of missing libraries: ", length(missing_libs)))
+  
+  
+  # Install the missing libraries and
+  # count the number of covered packages
+  
+  if (length(missing_libs) > 1){
     
     print("##########################")
     print("Attempting to install missing packages")
     print("##########################")
-
     
-    # install.packages(missing_libs, dependencies = TRUE, Ncpus = 2)
     
-    for (i in missing_libs){
-      if (!(i %in% alreadyInstalled)){
-        install.packages(i, dependencies = c("Imports", "Depends", "Suggests"))
-      }
-    }
+    install.packages(missing_libs, dependencies = TRUE, Ncpus = 4, dependencies = c("Imports", "Depends", "Suggests"))
+    
+    # for (i in missing_libs){
+    #   if (!(i %in% alreadyInstalled)){
+    #     install.packages(i, dependencies = c("Imports", "Depends", "Suggests"))
+    #   }
+    # }
     
     # Check how many packages got installed
     # for the current version of R
@@ -101,16 +103,16 @@ if (length(missing_libs) > 1){
     
     print(paste("Failed to install", length(failedPackage), "packages"))
     print(paste("Failed packages account for", length(failedModels), "models"))
-
-}
-
-else{
-  print("No missing libraries found")
-  print(paste(length(successPackages),  "required libraries already installed"))
-}
-
-print(paste("Total Models:", length(unique(successModels))))
-return(unique(successModels))
+    
+  }
+  
+  else{
+    print("No missing libraries found")
+    print(paste(length(successPackages),  "required libraries already installed"))
+  }
+  
+  print(paste("Total Models:", length(unique(successModels))))
+  return(unique(successModels))
 }
 
 
